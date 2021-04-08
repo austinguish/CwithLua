8f5aa86b181a0960fd25bd94ee6ccd615ae23a67
Jiang Yiwei <479772069@qq.com>
2021-04-08 14:24:57 +0800

    init the demo

---
+++ cmake-build-debug/CMakeFiles/CWithLua.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CWithLua.dir/include/luatest.cpp.o"
+  "CMakeFiles/CWithLua.dir/main.cpp.o"
+  "CWithLua"
+  "CWithLua.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CWithLua.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
